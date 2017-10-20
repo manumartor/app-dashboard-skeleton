1b41be4a874896fb193905fb880b0f77009fab84
Manu Martínez <martor@MacBook-Pro-de-Manu.local>
2017-10-20 19:18:21 +0200

    Upgrade vagrant dev enviroment to 1.1

--- Vagrantfile
+++ Vagrantfile
@@ -1,21 +1,56 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# All Vagrant configuration is done below. The "2" in Vagrant.configure
-# configures the configuration version (we support older styles for
-# backwards compatibility). Please don't change it unless you know what
-# you're doing.
+##################################
+###   MAIO CLOUD VAGRANTFILE   ###
+##################################
 Vagrant.configure("2") do |config|
-  # The most common configuration options are documented and commented below.
-  # For a complete reference, please see the online documentation at
-  # https://docs.vagrantup.com.
 
-  # Every Vagrant development environment requires a box. You can search for
-  # boxes at https://vagrantcloud.com/search.
+  # Define multiple machines:
+  #
+  # 1st machine is for the espos
+  #
+  # Machine name: app-skeleton.maio.test
+  # IP Address:   192.168.36.10
+  #
+  config.vm.define "app-skeleton" do |mch|
+    mch.vm.hostname = "app-skeleton.maio.test"
+    mch.vm.network "private_network", ip: "192.168.36.10"
+
+    # provisions
+    mch.vm.provision "shell", path: "bin/bootstrap.sh", args: "apache php composer phpunit nodejs bower gulp karma protractor"
+
+    mch.vm.provision "shell", inline: <<-SHELL
+        if ! grep -Fxq "alias execute-unittest" /home/ubuntu/.bashrc; then
+            echo "alias execute-unittest=/vagrant/bin/rununittest.sh" >> /home/ubuntu/.bashrc
+        fi
+        if ! grep -Fxq "alias execute-e2etest" /home/ubuntu/.bashrc; then
+            echo "alias execute-e2etest=/vagrant/bin/rune2etest.sh" >> /home/ubuntu/.bashrc
+        fi
+    SHELL
+
+    mch.vm.provision "shell", run: "always", inline: <<-SHELL
+      if [ -f "/etc/init.d/apache2" ]; then
+        /etc/init.d/apache2 restart
+      fi
+    SHELL
+    
+    # vm
+    mch.vm.provider "virtualbox" do |vb|
+      # Log place
+      vb.customize [ "modifyvm", :id, "--uartmode1", "file", File.join(Dir.pwd, "logs/ubuntu-xenial-16.04-vm_app-console.log") ]
+    end
+  end
+
+  # 
+  # CONTINUE WITH COMMONS SETTINGS
+  #
+
+  # set box
   config.vm.box = "ubuntu/xenial64"
-  config.vm.hostname = "app-desktop-skeleton"
 
-  required_plugins = %w( vagrant-vbguest vagrant-disksize )
+  # require plugins
+  required_plugins = %w( vagrant-vbguest vagrant-disksize landrush )
   _retry = false
   required_plugins.each do |plugin|
       unless Vagrant.has_plugin? plugin
@@ -23,64 +58,23 @@ Vagrant.configure("2") do |config|
           _retry=true
       end
   end
-
   if (_retry)
       exec "vagrant " + ARGV.join(' ')
   end
+
+  # adjust disk size
   config.disksize.size = "30GB"
 
-  config.ssh.forward_agent = true
-  config.ssh.forward_x11 = true
-
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine. In the example below,
-  # accessing "localhost:8080" will access port 80 on the guest machine.
-  # NOTE: This will enable public access to the opened port
-  config.vm.network "forwarded_port", guest: 80, host: 8080
-  config.vm.network "forwarded_port", guest: 3306, host: 33006
-
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine and only allow access
-  # via 127.0.0.1 to disable public access
-  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
-
-  # Create a private network, which allows host-only access to the machine
-  # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
-
-  # Create a public network, which generally matched to bridged network.
-  # Bridged networks make the machine appear as another physical device on
-  # your network.
-  # config.vm.network "public_network"
-
-  # Share an additional folder to the guest VM. The first argument is
-  # the path on the host to the actual folder. The second argument is
-  # the path on the guest to mount the folder. And the optional third
-  # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
-  config.vm.synced_folder "logs/apache2/", "/var/log/apache2", create: true
-  config.vm.synced_folder "logs/mysql/", "/var/log/mysql", create: true, id: "mysql", owner: 112, group: 117, mount_options: ["dmode=775,fmode=664"] # owner: "mysql", group: "mysql",
-
-  # Provider-specific configuration so you can fine-tune various
-  # backing providers for Vagrant. These expose provider-specific options.
-  # Example for VirtualBox:
-  #
+  # set dns landrush settings
+  config.landrush.enabled = true
+  config.landrush.tld = "maio.test"
+  
+  # set virtualbox settings
   config.vm.provider "virtualbox" do |vb|
     # Display the VirtualBox GUI when booting the machine
     vb.gui = false
 
     # Customize the amount of memory on the VM:
     vb.memory = "4096"
-  
-    # Log place
-    vb.customize [ "modifyvm", :id, "--uartmode1", "file", File.join(Dir.pwd, "logs/ubuntu-xenial-16.04-cloudimg-console.log") ]
   end
-  #
-  # View the documentation for the provider you are using for more
-  # information on available options.
-
-  # Enable provisioning with a shell script. Additional provisioners such as
-  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
-  # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", path: "bin/bootstrap.sh"
 end
--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,214 +1,428 @@
 #!/usr/bin/env bash
 
-LAMP_INSTALL=true
-NODEJS_INSTALL=true
-PHONEGAP_INSTALL=true
-COMPOSER_INSTALL=false
-PHPUNIT_INSTALL=false
-JMETER_INSTALL=false
+main() {
+  	echo "########################################"
+    echo "###   VAGRANT BOOTSRTAP FILE V.1.1   ###"
+	  echo "########################################"
 
-MYSQL_PASS=root
-MYSQL_CNF=/etc/mysql/mysql.conf.d/mysqld.cnf
-PHP_XDEBUGINI=/etc/php/7.0/mods-available/xdebug.ini
+  	set_enviroment
 
-TIMEZONE=Europe/Madrid
+  	echo ""
+  	echo ""
+  	echo "------------------------------------------"
+  	echo "------------------------------------------"
+  	echo " Installing: $@"
+  	echo "------------------------------------------"
+  	echo "------------------------------------------"
+  	echo ""
+  	echo ""
 
-# set pass
-#echo "vagrant" | passwd foo --stdin
+  	if echo $@ | grep -q -w apache; then
+    	apache
+  	fi
+  	if echo $@ | grep -q -w php; then
+    	php
+  	fi
+  	if echo $@ | grep -q -w composer; then
+    	composer
+  	fi
+  	if echo $@ | grep -q -w phpunit; then
+    	phpunit
+  	fi
+  	if echo $@ | grep -q -w mysql; then
+    	mysql
+    	mysqlclient
+  	fi
+  	if echo $@ | grep -q -w mysqlclient; then
+    	mysqlclient
+  	fi
+  	if echo $@ | grep -q -w mysqlprovisioning; then
+    	mysqlprovisioning
+  	fi
+  	if echo $@ | grep -q -w postgres; then
+    	postgres
+  	fi
+  	if echo $@ | grep -q -w postgresclient; then
+    	postgresclient
+  	fi
+  	if echo $@ | grep -q -w nodejs; then
+    	nodejs
+  	fi
+  	if echo $@ | grep -q -w bower; then
+    	bower
+  	fi
+  	if echo $@ | grep -q -w gulp; then
+    	gulp
+  	fi
+  	if echo $@ | grep -q -w karma; then
+    	karma
+  	fi
+  	if echo $@ | grep -q -w protractor; then
+    	protractor
+  	fi
+  	if echo $@ | grep -q -w jmeter; then
+    	jmeter
+  	fi
+    if echo $@ | grep -q -w arduinoide; then
+      arduinoide
+    fi
 
-# configure timezone
-timedatectl set-timezone $TIMEZONE
+  	finish_install
+}
 
-# configure init path
-if ! grep -Fxq "cd /vagrant" /home/ubuntu/.bashrc; then
-  echo "cd /vagrant
-	alias l='ls -lah'" >> /home/ubuntu/.bashrc
-fi
+set_enviroment() {
+  	echo '--> Setting enviroment...' 
 
-# Update server
-sudo apt-get -y update
-sudo apt-get -y upgrade
+  	TIMEZONE=Europe/Madrid
 
-# Install commons tools
-apt-get -y install build-essential g++ binutils-doc git subversion
-add-apt-repository ppa:webupd8team/sublime-text-2
-apt-get -y update
-apt-get install -y sublime-text default-jre
+  	# configure timezone
+  	timedatectl set-timezone $TIMEZONE
+  	echo '-> Setted timedate!'
 
-# Install LAMP
-if $LAMP_INSTALL; then
-  # Install Apache
-  apt-get install -y apache2
-  if ! [ -d /vagrant/public ]; then
-    mkdir /vagrant/public
-    echo "<?php phpinfo();" >> /vagrant/public/index.php
-  fi
-  if ! [ -f /var/www/html/ ]; then
-    rm -rf /var/www/html
-    ln -fs /vagrant/public/ /var/www/html
-  fi
+  	# configure init path
+  	if ! grep -Fxq "cd /vagrant" /home/ubuntu/.bashrc; then
+      	echo "cd /vagrant
+alias l='ls -lah'" >> /home/ubuntu/.bashrc
+    	echo '-> Setted init path in .bashrc!'
+  	fi
 
-  # Set Apache Logs
-  if ! [ -d /var/log/apache2/ ]; then
-    sudo rm -rf /var/log/apache2/
-    ln -fs /vagrant/logs/apache2 /var/log/apache2
-  fi
-  echo "-->Apache2 installed!!"
+  	# Update server
+  	sudo apt-get -y update
+ 	sudo apt-get -y upgrade
+ 	echo '-> apt-get updraded!'
 
-  #Install PHP
-  apt-get install -y php libapache2-mod-php php-cli php-mysql php-curl php-gd php-intl php-ldap php-mbstring php-soap php-sqlite3 php-xmlrpc php-xdebug
+  	# Install commons tools
+  	apt-get -y install build-essential g++ binutils-doc git subversion
+  	echo '-> installed commons tools!'
 
-  #Configure PHP
-  if ! [ -f /etc/php/7.0/apache2/conf.d/1-general.ini ]; then
-    echo "; configuration for general php
-  ; priority=1
-  memory_limit = 512M
-  display_errors = On
-  post_max_size = 50M
-  upload_max_filesize = 50M
-  " >> /etc/php/7.0/apache2/conf.d/1-general.ini
-  fi
-  if ! [ -f /etc/php/7.0/cli/conf.d/1-general.ini ]; then
-    echo "; configuration for general php
-  ; priority=1
-  memory_limit = 512M
-  display_errors = On
-  " >> /etc/php/7.0/cli/conf.d/1-general.ini
-  fi
-  echo "xdebug.show_error_trace = 1" >> $PHP_XDEBUGINI
-  echo "xdebug.remote_enable = 1" >> $PHP_XDEBUGINI
-  echo "xdebug.remote_connect_back=1" >> $PHP_XDEBUGINI
-  echo "xdebug.remote_port=9000" >> $PHP_XDEBUGINI
-  echo "xdebug.remote_host=10.0.2.2" >> $PHP_XDEBUGINI
+  	echo '--> Finish setting enviroment...'
 
-  echo "-->PHP Installed!!"
+    # Create logs dirs
+    if ! [ -d /vagrant/logs ]; then
+        mkdir /vagrant/logs
+    fi
+}
 
-  # Install MySQL
-  echo "mysql-server mysql-server/root_password password $MYSQL_PASS" | debconf-set-selections
-  echo "mysql-server mysql-server/root_password_again password $MYSQL_PASS" | debconf-set-selections
-  apt-get install -y mysql-client mysql-server
+apache() {
+  	# Install Apache
+  	apt-get -y install apache2
 
-  # Configure MySQL
-  sed -i -e "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" $MYSQL_CNF
-  sed -i -e "s/#general_log/general_log/g" $MYSQL_CNF
-  echo "slow_query_log = 1" >> $MYSQL_CNF
-  sed -i -e "s/#log_slow_queries/slow_query_log_file/" $MYSQL_CNF
-  sed -i -e "s/#long_query_time\s*=\s*2/long_query_time = 1/" $MYSQL_CNF
-  sed -i -e "s/#log-queries-not-using-indexes/log-queries-not-using-indexes/" $MYSQL_CNF
+  	if ! [ -d /vagrant/public ]; then
+  	  	mkdir /vagrant/public
+    	echo "<?php phpinfo();" >> /vagrant/public/index.php
+  	fi
 
-  # Allow root access from any host
-  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION" | mysql -u root --password=$MYSQL_PASS
-  echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=$MYSQL_PASS
+  	sed -i "s/^\(.*\)www-data/\1ubuntu/g" /etc/apache2/envvars
+  	chown -R ubuntu:ubuntu /var/log/apache2
 
-  # Executing all SQL files in /vagrant/provision-sql folder
-  if ! [ -d "/vagrant/bin/provision-sql" ]; then
-    mkdir /vagrant/bin/provision-sql
-    #echo "# blank sql bootstrap script" >> /vagrant/bin/provision-sql/blank.sql
-  fi
-  if [ -d "/vagrant/bin/provision-sql" ]; then
-    echo "Executing all SQL files in /vagrant/provision-sql folder ..."
-    echo "-------------------------------------"
-    for sql_file in /vagrant/bin/provision-sql/*.sql
-    do
-      echo "EXECUTING $sql_file..."
-      time mysql -u root --password=$MYSQL_PASS < $sql_file
-      echo "FINISHED $sql_file"
-      echo ""
-    done
-  fi
+  	if [ ! -f "/etc/apache2/sites-available/vagrant_vhost.conf" ]; then
+    	cat << EOF > /etc/apache2/sites-available/vagrant_vhost.conf
+<VirtualHost *:80>
+  DocumentRoot /vagrant/public
+  LogLevel debug
 
-  service mysql restart
-  echo "-->MySQL installed!! pass: $MYSQL_PASS"
+  ErrorLog /var/log/apache2/vagrant-error.log
+  CustomLog /var/log/apache2/vagrant-access.log combined
 
-  # Restart Apache service
-  service apache2 restart
-fi
+  <Directory /vagrant/public>
+      AllowOverride All
+      Require all granted
+  </Directory>
+</VirtualHost>
+EOF
+  	fi
 
-# Install nodejs
-if $NODEJS_INSTALL; then  
-  apt-get install -y python-software-properties python g++ make
-  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-  # curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-  apt-get install -y nodejs
+	a2dissite 000-default
+	a2ensite vagrant_vhost
 
-  # Update npm
-  npm i -g npm
-  
-  # Install bower globally
-  npm install -g bower
+	a2enmod rewrite
 
-  # Install gulp globally
-  npm install -g gulp
+	/etc/init.d/apache2 restart
+	update-rc.d apache2 enable
 
-  # Install karma globally
-  apt-get install -y xvfb xauth x11-apps chromium-browser firefox
-  if ! [ -f /etc/profile.d/custom.sh ]; then
-    # Se recuerda que es necesario tener instalado X11 (osx: xquartz)
-    echo "#Xvfb :99 > /home/ubuntu/xvfb_output.log&
-#export DISPLAY=:99
+	echo "--> Apache2 installed!!"
+}
+
+php() {
+	#Install PHP
+	PHP_XDEBUGINI=/etc/php/7.0/mods-available/xdebug.ini
+
+  	apt-get install -y php libapache2-mod-php php-cli php-mysql php-curl php-gd php-intl php-ldap php-mbstring php-soap php-sqlite3 php-xmlrpc php-xdebug
+
+  	#Configure PHP
+  	if ! [ -f /etc/php/7.0/apache2/conf.d/1-general.ini ]; then
+    	echo "; configuration for general php
+; priority=1
+memory_limit = 512M
+display_errors = On
+post_max_size = 50M
+upload_max_filesize = 50M
+" >> /etc/php/7.0/apache2/conf.d/1-general.ini
+  	fi
+  	if ! [ -f /etc/php/7.0/cli/conf.d/1-general.ini ]; then
+    	echo "; configuration for general php
+; priority=1
+memory_limit = 512M
+display_errors = On
+" >> /etc/php/7.0/cli/conf.d/1-general.ini
+	fi
+	echo "xdebug.show_error_trace = 1" >> $PHP_XDEBUGINI
+	echo "xdebug.remote_enable = 1" >> $PHP_XDEBUGINI
+	echo "xdebug.remote_connect_back=1" >> $PHP_XDEBUGINI
+	echo "xdebug.remote_port=9000" >> $PHP_XDEBUGINI
+	echo "xdebug.remote_host=10.0.2.2" >> $PHP_XDEBUGINI
+
+	echo "--> PHP Installed!!"
+
+}
+
+composer() {
+  	# Install composer
+  	apt-get install -y composer
+    echo "--> Composer installed!!"
+}
+
+phpunit() {
+    if ! [ -f /usr/local/bin/phpunit ]; then
+	  	wget -q https://phar.phpunit.de/phpunit.phar
+	  	chmod +x phpunit.phar 
+	  	mv phpunit.phar /usr/local/bin/phpunit
+
+	  	# check exists save reports dir
+		if ! [ -d "/vagrant/logs/testreports" ]; then
+			mkdir /vagrant/logs/testreports
+		fi
+
+	  	echo "--> Phpunit installed!!"
+    fi
+}
+
+mysql() {
+	# Install MySQL
+	MYSQL_PASS=root
+	MYSQL_CNF=/etc/mysql/mysql.conf.d/mysqld.cnf
+
+	echo "mysql-server mysql-server/root_password password $MYSQL_PASS" | debconf-set-selections
+	echo "mysql-server mysql-server/root_password_again password $MYSQL_PASS" | debconf-set-selections
+	apt-get install -y mysql-client mysql-server
+
+	# Configure MySQL
+	sed -i -e "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" $MYSQL_CNF
+	sed -i -e "s/#general_log/general_log/g" $MYSQL_CNF
+	echo "slow_query_log = 1" >> $MYSQL_CNF
+	sed -i -e "s/#log_slow_queries/slow_query_log_file/" $MYSQL_CNF
+	sed -i -e "s/#long_query_time\s*=\s*2/long_query_time = 1/" $MYSQL_CNF
+	sed -i -e "s/#log-queries-not-using-indexes/log-queries-not-using-indexes/" $MYSQL_CNF
+
+	# Allow root access from any host
+	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION" | mysql -u root --password=$MYSQL_PASS
+	echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=$MYSQL_PASS
+
+	# Restart Mysql service
+	service mysql restart
+
+	# Restart Apache service
+    service apache2 restart
+
+    echo "--> MySQL installed!! pass: $MYSQL_PASS"
+
+}
+
+mysqlclient() {
+	# Install MySQL Client
+	apt-get install -y mysql-client
+
+	echo "--> MySQL Client installed!!"
+}
+
+mysqlprovisioning() {
+  	# Executing all SQL files in /vagrant/provision-sql folder
+  	if ! [ -d "/vagrant/bin/provision-sql" ]; then
+    	mkdir /vagrant/bin/provision-sql
+    	#echo "# blank sql bootstrap script" >> /vagrant/bin/provision-sql/blank.sql
+  	fi
+  	if [ -d "/vagrant/bin/provision-sql" ]; then
+	    echo "Executing all SQL files in /vagrant/provision-sql folder ..."
+	    echo "-------------------------------------"
+	    for sql_file in /vagrant/bin/provision-sql/*.sql
+	    do
+	        echo "EXECUTING $sql_file..."
+	        time mysql -u root --password=$MYSQL_PASS < $sql_file
+	        echo "FINISHED $sql_file"
+	        echo ""
+    	done
+
+    	echo "--> MySQL provisioned!!"
+  	fi
+}
+
+postgres() {
+  	# Install postgress
+  	apt-get -y install postgresql postgresql-contrib
+  	echo "alias psql='sudo -u postgres psql'" >> $BASHRC_PATH
+
+  	# Config postgres
+  	sed -i -e "s/127.0.0.1\/32/192.168.50.1\/24/" /etc/postgresql/9.5/main/pg_hba.conf
+  	sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.5/main/postgresql.conf
+  	sed -i -e "s/#log_destination/log_destination/" /etc/postgresql/9.5/main/postgresql.conf
+  	sed -i -e "s/#logging_collector = off/logging_collector = on/" /etc/postgresql/9.5/main/postgresql.conf
+  	sed -i -e "s/#log_directory = 'pg_log'/log_directory = '\/var\/log\/postgresql'/" /etc/postgresql/9.5/main/postgresql.conf
+  	sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+  	# create aulaxxi database
+  	sudo -u postgres psql -c "CREATE USER aulaxxi_m332 PASSWORD 'aulaxxim3322017';"
+  	sudo -u postgres psql -c "CREATE DATABASE aulaxxi_m332 OWNER 'aulaxxi_m332';"
+  	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE aulaxxi_m332 TO aulaxxi_m332;"
+  	sudo -u postgres psql -c "CREATE DATABASE aulaxxi_m332_phpu OWNER 'aulaxxi_m332';"
+  	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE aulaxxi_m332_phpu TO aulaxxi_m332;"
+
+  	echo "Created database and user aulaxxi_m332"
+
+  	# importing data
+  	if ! [ -d "/vagrant/bin/provision-pgsql" ]; then
+    	mkdir /vagrant/bin/provision-pgsql
+  	fi
+  	if [ -d "/vagrant/bin/provision-pgsql" ]; then
+      	echo "Executing all SQL files in /vagrant/bin/provision-pgsql folder ..."
+      	echo "-------------------------------------"
+      	for sql_file in /vagrant/bin/provision-pgsql/*.pgsql
+      	do
+        	echo "EXECUTING $sql_file..."
+          	time sudo -u postgres psql aulaxxi_m332 < $sql_file
+          	echo "FINISHED $sql_file"
+          	echo ""
+      	done
+  	fi
+
+  	service postgresql restart
+  	update-rc.d postgresql enable
+    
+  	echo "-->Postgresql Installed!!"
+}
+
+postgresclient() {
+  	# Install Postgres client
+    apt-get -y install postgresql-client-common postgresql-client-9.5
+
+    echo "-->Postgresql Client Installed!!"
+}
+
+nodejs() {
+  	# Install nodejs
+  	apt-get install -y python-software-properties python g++ make
+    
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+    # curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+    apt-get install -y nodejs
+
+    # Update npm
+    npm i -g npm
+
+    echo "--> NodeJS installed!!"
+}
+
+bower() {
+  	# Install bower globally
+    npm install -g bower
+
+    echo "--> Bower installed!!"
+}
+
+gulp() {
+  	# Install gulp globally
+    npm install -g gulp
+
+    echo "--> Gulp installed!!"
+}
+
+karma() {
+  	# Install karma globally
+    apt-get install -y xvfb chromium-browser firefox
+
+    if ! [ -f /etc/profile.d/custom.sh ]; then
+      echo "export DISPLAY=:99.0
 export CHROME_BIN=/usr/bin/chromium-browser
 export FIREFOX_BIN=/usr/bin/firefox
 
-alias app-rununittest=/vagrant/bin/rununittest.sh
-alias app-rune2etest=/vagrant/bin/rune2etest.sh
+alias cloud-runallunittest=/vagrant/bin/rununittest.sh
+alias cloud-runmaiomanunittest='cd /vagrant/public/maioman && npm test'" > /etc/profile.d/custom.sh
+    fi
+    npm install -g karma karma-cli  
+    npm install -g jasmine-core jasmine-node jasmine-reporters karma-jasmine karma-chrome-launcher karma-firefox-launcher karma-coverage karma-junit-reporter
 
-alias sublime-text='nohup sublime-text $1 >/dev/null 2>&1 &'" > /etc/profile.d/custom.sh
-  fi
-  npm install -g karma karma-cli  
-  npm install -g jasmine-core jasmine-node jasmine-reporters karma-jasmine karma-chrome-launcher karma-firefox-launcher karma-coverage karma-junit-reporter
+    # check exists save reports dir
+    if ! [ -d "/vagrant/logs/testreports" ]; then
+    	mkdir /vagrant/logs/testreports
+  	fi
 
-  # Install protractor globally
-  npm install -g protractor
-  if ! [ -d "/vagrant/logs/e2etest_reports" ]; then
-    mkdir /vagrant/logs/e2etest_reports
-  fi
+    echo "-->Karma installed!!"
+}
 
-  echo "--> nodejs installed!!"
-fi
+protractor() {
+  	# Install protractor globally
+    npm install -g protractor
 
-# Install phonegap
-if $PHONEGAP_INSTALL; then
-  npm install -g phonegap
+    # check exists save reports dir
+    if ! [ -d "/vagrant/logs/testreports" ]; then
+    	mkdir /vagrant/logs/testreports
+  	fi
 
-  wget http://dl.google.com/android/android-sdk_r24.2-linux.tgz
-  tar -xvf android-sdk_r24.2-linux.tgz
-  cd android-sdk-linux/tools
-  yes | ./android update sdk --no-ui
-  echo "export PATH=${PATH}:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools
-export ANDROID_HOME=~/android-sdk-linux" >> /home/ubuntu/.bashrc
+    echo "-->protractor installed!!"
+}
 
-  echo "--> phonegao installed!!"
-fi
+jmeter() {
+  	# Install jmeter
+  	if ! [ -d /usr/local/lib/apache-jmeter ]; then
+    		apt-get install -y default-jre
 
-# Install composer
-if $COMPOSER_INSTALL; then 
-  apt-get install -y composer
-  echo "--> composer installed!!"
-fi
+    		wget -q http://redrockdigimark.com/apachemirror/jmeter/binaries/apache-jmeter-3.1.tgz
+    		tar -xf apache-jmeter-3.1.tgz
+    		mv apache-jmeter-3.1/ /usr/local/lib/apache-jmeter
 
-# Install phpunit
-if $PHPUNIT_INSTALL; then
-  if ! [ -f /usr/local/bin/phpunit ]; then
-    wget -q https://phar.phpunit.de/phpunit.phar
-    chmod +x phpunit.phar 
-    mv phpunit.phar /usr/local/bin/phpunit
-  fi
-  if ! [ -d "/vagrant/logs/unittest_reports" ]; then
-    mkdir /vagrant/logs/unittest_reports
-  fi
-  echo "--> phpunit installed!!"
-fi
+    		ln -fs /usr/local/lib/apache-jmeter/bin/jmeter /usr/local/bin/jmeter
+    		ln -fs /usr/local/lib/apache-jmeter/bin/jmeter-server /usr/local/bin/jmeter-server
 
-# Install jmeter
-if $JMETER_INSTALL; then
-  if ! [ -d /usr/local/lib/apache-jmeter ]; then
-    wget -q http://redrockdigimark.com/apachemirror/jmeter/binaries/apache-jmeter-3.1.tgz
-    tar -xf apache-jmeter-3.1.tgz
-    mv apache-jmeter-3.1/ /usr/local/lib/apache-jmeter
-    ln -fs /usr/local/lib/apache-jmeter/bin/jmeter /usr/local/bin/jmeter
-    ln -fs /usr/local/lib/apache-jmeter/bin/jmeter-server /usr/local/bin/jmeter-server
-  fi
-  echo "-->jmeter installed!!"
-fi
+    		# check exists save reports dir
+  	    if ! [ -d "/vagrant/logs/testreports" ]; then
+  	    	  mkdir /vagrant/logs/testreports
+  	  	fi
 
-apt-get -y autoremove
+        echo "--> Jmeter installed!!"
+    fi
+}
+
+finish_install() {
+  	apt-get -y autoremove
+  	echo '---> Boostrap API finished!!'
+}
+
+arduinoide() {
+    echo "--> Installing ArduinoIDE 1.5.7"
+
+    if ! [ -d /opt/arduino-ide ]; then
+        # install picocom for serial port communication
+        apt-get install -y picocom default-jre
+
+        # install arduino
+        wget http://downloads.arduino.cc/arduino-1.5.7-linux64.tgz
+        tar -zxvf arduino-1.5.7-linux64.tgz
+        mv arduino-1.5.7 /opt/arduino-ide
+        chmod +x /opt/arduino-ide/arduino
+
+        ln -s /opt/arduino-ide/arduino /usr/local/bin/arduino
+    fi
+
+    echo "--> ArduinoIDE 1.5.7 Installed"
+}
+
+main $@
+exit 0
+
+# Builtin variables:
+# There are some useful builtin variables, like
+echo "Last program's return value: $?"
+echo "Script's PID: $$"
+echo "Number of arguments passed to script: $#"
+echo "All arguments passed to script: $@"
+echo "Script's arguments separated into different variables: $1 $2..."
